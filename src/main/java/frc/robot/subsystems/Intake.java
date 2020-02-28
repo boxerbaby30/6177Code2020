3f69b3098aa5eac6640a7f880e53ab9139df014b
genspect <ggamer4590@gmail.com>
2020-02-28 18:49:11 -0500

    2/28 EOD

---
+++ src/main/java/frc/robot/subsystems/Intake.java
@@ -0,0 +1,31 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2018 FIRST. All Rights Reserved.                             */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package frc.robot.subsystems;
+
+import com.ctre.phoenix.motorcontrol.can.TalonSRX;
+import edu.wpi.first.wpilibj.DoubleSolenoid;
+/**
+ * 
+ * Add your docs here.
+ */
+public class Intake {
+  private TalonSRX inmotor;
+  private DoubleSolenoid pumpthing;
+  // Put methods for controlling this subsystem
+  // here. Call these from Commands.
+  public Intake(){
+
+    TalonSRX inmotor = new TalonSRX(1);
+    DoubleSolenoid pumpthing = new DoubleSolenoid(1, 2);
+
+  }
+  
+    (talon name).setSpeed(.7)
+
+
+}
